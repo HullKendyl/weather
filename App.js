a52afec1ebaf1a6fe3500365249ac45821ac7ce1
Kendyl Hull <hullkendyl@gmail.com>
2023-10-04 06:50:11 -0400

    Removing OurChildPage

--- App.js
+++ App.js
@@ -3,17 +3,13 @@ import { StyleSheet, View, Text } from "react-native";
 import CurrentWeather from "./src/components/CurrentWeather";
 import UpcomingWeather from "./src/components/UpcomingWeather";
 import CityDetails from "./src/components/CityDetails";
-import OurChild from "./src/components/OurChild";
 
 const App = () => {
   return (
-    <View>
-      <OurChild message={'Hello'}/>
-      <OurChild message={'Greetings'}/>
-      <OurChild message={'Goodbye'}/>
+    <View style={styles.container}>
+      <UpcomingWeather />
     </View>
     //<CurrentWeather />
-    // <UpcomingWeather />
   )
 }
 
