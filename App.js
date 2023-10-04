8154c8877fb649588a3c4288e11c74c722b22741
Kendyl Hull <hullkendyl@gmail.com>
2023-10-04 06:43:37 -0400

    Created OurChild component

--- App.js
+++ App.js
@@ -3,12 +3,25 @@ import { StyleSheet, View, Text } from "react-native";
 import CurrentWeather from "./src/components/CurrentWeather";
 import UpcomingWeather from "./src/components/UpcomingWeather";
 import CityDetails from "./src/components/CityDetails";
+import OurChild from "./src/components/OurChild";
 
 const App = () => {
   return (
+    <View>
+      <OurChild message={'Hello'}/>
+      <OurChild message={'Greetings'}/>
+      <OurChild message={'Goodbye'}/>
+    </View>
     //<CurrentWeather />
-    <UpcomingWeather />
+    // <UpcomingWeather />
   )
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+})
+
 export default App;
+
