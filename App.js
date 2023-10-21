08ae25a88e09b0488fcf14ba842c3b9619819a2d
Kendyl Hull <hullkendyl@gmail.com>
2023-10-21 13:20:59 -0400

    Created city screen with basic content

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { StyleSheet, View, Text } from "react-native";
 import CurrentWeather from "./src/screens/CurrentWeather";
 import UpcomingWeather from "./src/screens/UpcomingWeather";
+import City from "./src/screens/City";
 import CityDetails from "./src/components/CityDetails";
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <UpcomingWeather />
+      {/* <UpcomingWeather /> */}
+      <City />
     </View>
     //<CurrentWeather />
   )
--- src/screens/City.js
+++ src/screens/City.js
@@ -1,14 +1,71 @@
 import React from "react";
-import { View, Text, SafeAreaView, StatusBar } from 'react-native';
+import { View, Text, SafeAreaView, StatusBar, StyleSheet, ImageBackground } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
 function City () {
     return (
-        <SafeAreaView>
-            <View>
-                <Text>City</Text>
-            </View>
+        <SafeAreaView style={styles.container}>
+            <ImageBackground source={require('../../assets/city-background.jpg')} style={styles.imageLayout}>
+                <Text style={[styles.cityName, styles.cityText]}>London</Text>
+                <Text style={[styles.countryName, styles.cityText]}>United Kingdom</Text>
+                <View style={styles.populationWrapper}>
+                    <Feather name={'user'} size={50} color={'red'}/>
+                    <Text style={styles.populationText}>8000</Text>
+                </View>
+                <View style={styles.sunWrapper}>
+                    <Feather name={'sunrise'} size={50} color={'white'}/> 
+                    <Text style={styles.sunText}>10:46:58am</Text>
+                    <Feather name={'sunset'} size={50} color={'white'}/> 
+                    <Text style={styles.sunText}>17:28:15pm</Text>
+                </View>
+            </ImageBackground>
         </SafeAreaView> 
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight || 0
+    },
+    imageLayout: {
+        flex: 1
+    },
+    cityName: {
+        fontSize: 40,
+    },
+    countryName: {
+        fontSize: 30,
+    },
+    cityText: {
+        justifyContent: 'center',
+        alignSelf: 'center',
+        fontWeight: 'bold',
+        color: 'white' 
+    },
+    populationText: {
+        fontSize: 25,
+        marginLeft: 7.5,
+        color: 'red',
+        fontWeight: 'bold'
+    },
+    sunText: {
+        fontSize: 20,
+        color: 'white',
+        fontWeight: 'bold'
+    },
+    populationWrapper: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 30
+    },
+    sunWrapper: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        marginTop: 30
+    }
+});
 
 export default City;
